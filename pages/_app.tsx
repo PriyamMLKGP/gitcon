6cfffe7cce50478e27f64f016a1d99ddd90b29e9
Priyam Goenka <priyam9800@gmail.com>
2022-08-31 18:32:39 +0530

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,27 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-
+import Header from '../components/Header'
+import {SessionProvider} from 'next-auth/react'
+import  AuthWrapper from '../components/AuthWrapper'
+import Link from 'next/link'
+import Dashboard from './Dashboard'
+import { useRouter } from 'next/router'
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const router=useRouter()
+  return (
+  <SessionProvider session={pageProps.session}>
+    {router.pathname==="/"?
+    (<>
+    <Header/>
+    <Component {...pageProps} /></>
+    )
+    :
+    (<AuthWrapper>
+      <Dashboard/>
+    </AuthWrapper>)
+    } 
+  </SessionProvider>
+  )
 }
 
 export default MyApp
