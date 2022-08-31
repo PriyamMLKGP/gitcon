e170897d52b48680e3491c00c21cfd5d028efab2
Priyam Goenka <priyam9800@gmail.com>
2022-08-31 18:58:04 +0530

    undefned assigend to string error resolved

--- pages/api/auth/[...nextauth].tsx
+++ pages/api/auth/[...nextauth].tsx
@@ -1,14 +1,13 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github"
 
-
 console.log("server render check True ")
 
 export default NextAuth({
     providers: [
         GithubProvider({
-            clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET,
+            clientId: process.env.GITHUB_ID===undefined?'':process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET===undefined?'':process.env.GITHUB_SECRET,
         }),
     ],
 })
